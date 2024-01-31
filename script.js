243ab4b39c11720a407ef2245d478115aab86076
cevu <devcevu@gmail.com>
2024-01-31 16:49:05 +0100

    Finilize style, Add % operator and . operator, Fix bug with . (multiple instances of .)

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const screen = document.querySelector("#screen");
 let history = document.querySelector("#history");
-const btns = document.querySelectorAll("button");
+const btns = document.querySelectorAll("#btn");
 let firstNum;
 let secondNum;
 let operator;
 
 btns.forEach((btn) => {
 	btn.addEventListener("click", () => {
-		if (
+		if (btn.value === "+/-") {
+			screen.value = -screen.value;
+		} else if (
 			btn.value === "+" ||
 			btn.value === "-" ||
-			btn.value === "*" ||
-			btn.value === "/"
+			btn.value === "x" ||
+			btn.value === "/" ||
+			btn.value === "%"
 		) {
 			firstNum = screen.value;
 			screen.value = "";
 			operator = btn.value;
-			history.value = firstNum + operator;
+			history.value = `${firstNum} ${operator}`;
 		} else if (btn.value === "clear") {
 			screen.value = "";
 			firstNum = 0;
@@ -25,10 +28,16 @@ btns.forEach((btn) => {
 			history.value = "";
 		} else if (btn.value === "=") {
 			secondNum = screen.value;
-			history.value = firstNum + operator + secondNum;
+			history.value = `${firstNum} ${operator} (${secondNum})`;
 			screen.value = operate(operator, firstNum, secondNum);
 			firstNum = operate(operator, firstNum, secondNum);
 		} else {
+			if (btn.value === ".") {
+				if (screen.value.includes(".")) {
+					screen.value = screen.value.substring(0, screen.value.length - 1);
+				}
+				screen.value = screen.value - 0;
+			}
 			screen.value += btn.value;
 		}
 	});
@@ -44,7 +53,10 @@ function multiply(a, b) {
 	return a * b;
 }
 function divide(a, b) {
-	return a / b;
+	return (a / b).toFixed(2);
+}
+function percent(a, b) {
+	return a % b;
 }
 function operate(operator, a, b) {
 	let firstNum = Number(a);
@@ -54,11 +66,13 @@ function operate(operator, a, b) {
 			return add(firstNum, secondNum);
 		case "-":
 			return subtract(firstNum, secondNum);
-		case "*":
+		case "x":
 			return multiply(firstNum, secondNum);
 		case "/":
 			if (b === 0) return null;
 			else return divide(firstNum, secondNum);
+		case "%":
+			return percent(firstNum, secondNum);
 		default:
 			return null;
 	}
